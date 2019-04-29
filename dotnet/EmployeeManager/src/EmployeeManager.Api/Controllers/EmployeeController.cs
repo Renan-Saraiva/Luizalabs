4a8b178b25705a817db1023f7ccfd7e6eb8be338
Renan Magalhães Saraiva <renan.m.saraiva@gmail.com>
2019-04-28 21:51:26 -0300

    Criação da aplicação do desafio em .NET

---
+++ dotnet/EmployeeManager/src/EmployeeManager.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeManager.Domain.Models;
+
+namespace EmployeeManager.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET employee
+        [HttpGet]
+        public ActionResult<Employees> Get()
+        {
+            return new Employees {
+                new Employee {
+                    name = "Arnaldo Pereira",
+                    email = "arnaldo@luizalabs.com",
+                    department = "Architecture"
+                },
+                new Employee {
+                    name = "Arnaldo Pereira",
+                    email = "arnaldo@luizalabs.com",
+                    department = "Architecture"
+                },
+                new Employee {
+                    name = "Arnaldo Pereira",
+                    email = "arnaldo@luizalabs.com",
+                    department = "Architecture"
+                },
+            };
+        }
+
+        // GET employee/5
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            return new Employee
+            {
+                name = "Arnaldo Pereira",
+                email = "arnaldo@luizalabs.com",
+                department = "Architecture"
+            };
+        }
+
+        // POST employee
+        [HttpPost]
+        public void Post([FromBody] Employee value)
+        {
+
+        }
+
+        // PUT employee/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE employee/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
---
+++ dotnet/EmployeeManager/src/EmployeeManager.Domain/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager.Domain.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string department { get; set; }
+    }
+
+    public class Employees : List<Employee> { }
+}
